db7ffbfe310ab2652eff859c22f063bdadec9a47
ceenlrnjim <kirkwojl@hotmail.com>
2012-11-12 10:35:17 -0500

    Update src/logregvec.clj
    Changed implementation to be based on a supplied gradient function

--- src/logregvec.clj
+++ src/logregvec.clj
@@ -2,53 +2,58 @@
   (:require [incanter.core :as alg]))
 
 
-; Utility functions -----------------------------------------------
-; TODO: move to utility package - check for duplicates in other libraries
-(defn feature-count [X]
-  {:pre [(alg/matrix? X)] }
-  (second (alg/dim X)))
-
-(defn uniform-vector [size val]
-  (alg/matrix (map (fn [_] (vector val)) (range 0 size))))
-
-(defn empty-vector [size]
-  (uniform-vector size 0))
-
 ; Calculations functions -----------------------------------------------
-
-(defn hyperror [hypFn X yvec]
-  (alg/minus (hypFn X) yvec))
-
-; TODO: change gradients to be arguments (as in fminunc etc.) to support standard interface for minimization
-(defn dJdtheta [hypFn X yvec]
-  (alg/mmult (alg/trans X) (hyperror hypFn X yvec)))
-
-(defn reductionamt [hypFn X yvec alpha]
-  (alg/mult (/ alpha (count yvec)) (dJdtheta hypFn X yvec)))
-
 (defn next-thetas
-  [hypothesis thetas alpha X yvec]
-  (alg/minus thetas (reductionamt (partial hypothesis thetas) X yvec alpha)))
+  "Computes the next value of theta given the gradients and current theta values"
+  [thetas learning-rate gradients]
+  (alg/minus 
+    thetas 
+    (alg/mult learning-rate (gradients thetas))))
 
 
 ; API functions -----------------------------------------------
-(defn optimize [X y alpha maxiters hypothesis]
+; TODO: add regularization support
+; TODO: where do I need the cost function? Just to track progress?
+; TODO: change to map based arguments for clarity?
+(defn optimize [initial-thetas learning-rate costfn gradientfn maxiters]
   (loop [iters maxiters
-       thetas (empty-vector (feature-count X))]
+         thetas initial-thetas]
+    ;(println "Iteration: " iters " cost=" (costfn thetas))
     (if (= 0 iters) thetas
       (recur 
         (dec iters)
-        (next-thetas hypothesis thetas alpha X y)))))
+        (next-thetas thetas learning-rate gradientfn)))))
 
 
 ; Algorithm functions -----------------------------------------------
-(defn sigmoid [X]
+(defn sigmoid
   "Returns the value of the sigmoid function for each value in the matrix X"
+  [X]
   (alg/matrix-map #(/ 1 (+ 1 (alg/exp (* -1 %)))) X))
 
-(defn logistic-hypothesis [thetas X]
+(defn logistic-hypothesis 
   "Given parameters thetas, returns the predicted value for all the inputs in the training set matrix X"
+  [thetas X]
   (sigmoid (alg/mmult X thetas)))
 
-(defn linear-hypothesis [thetas X]
+(defn linear-hypothesis
+  "Given parameters thetas, returns the predicted value for all the inputs in the training set matrix X which is
+   expected to have a row for each example with a column for each feature"
+  [thetas X]
   (alg/mmult X thetas))
+
+(defn linlog-cost
+  "returns the cost for the specified thetas and hypothsis function using the squared error"
+  [hypfn X y]
+  (fn [thetas]
+    (let [[m n] (alg/dim X)]
+      (alg/mult (/ 1 (* 2 m)) (alg/pow (alg/minus (hypfn thetas X) y))))))
+
+(defn linlog-gradients 
+  "Returns a function of parameters theta that return the gradient value relative to each feature xj"
+  [hypfn X y]
+  (fn [thetas]
+    (let [[setsize features] (alg/dim X)
+          predicted (hypfn thetas X)
+          errors (alg/minus predicted y)]
+      (alg/mult (/ 1 setsize) (alg/mmult (alg/trans X) errors)))))
