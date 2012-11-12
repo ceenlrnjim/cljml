cc420a391a15ea324b00a9a663fd843ed371e22c
ceenlrnjim <kirkwojl@hotmail.com>
2012-11-12 09:33:12 -0500

    Update src/logregvec.clj
    removed testing code and updated feature count implementation

--- src/logregvec.clj
+++ src/logregvec.clj
@@ -3,8 +3,10 @@
 
 
 ; Utility functions -----------------------------------------------
-(defn featurecount [X]
-  (count (first X)))
+; TODO: move to utility package - check for duplicates in other libraries
+(defn feature-count [X]
+  {:pre [(alg/matrix? X)] }
+  (second (alg/dim X)))
 
 (defn uniform-vector [size val]
   (alg/matrix (map (fn [_] (vector val)) (range 0 size))))
@@ -17,6 +19,7 @@
 (defn hyperror [hypFn X yvec]
   (alg/minus (hypFn X) yvec))
 
+; TODO: change gradients to be arguments (as in fminunc etc.) to support standard interface for minimization
 (defn dJdtheta [hypFn X yvec]
   (alg/mmult (alg/trans X) (hyperror hypFn X yvec)))
 
@@ -31,7 +34,7 @@
 ; API functions -----------------------------------------------
 (defn optimize [X y alpha maxiters hypothesis]
   (loop [iters maxiters
-       thetas (empty-vector (featurecount X))]
+       thetas (empty-vector (feature-count X))]
     (if (= 0 iters) thetas
       (recur 
         (dec iters)
@@ -49,25 +52,3 @@
 
 (defn linear-hypothesis [thetas X]
   (alg/mmult X thetas))
-
-; TODO: move all this stuff out and make unit tests
-; Execute a logistic test
-(def myX (alg/matrix [[1 0 0] [1 0 1] [1 1.5 1] [1 1.5 2] [1 0 2] [1 0 3] [1 1 0] [1 1 1] [1 1 2] [1 1 3] [1 2 0] [1 2 1] [1 2 2] [1 2 3]]))
-(def myy (alg/matrix [[0] [0] [0] [0] [0] [0] [0] [0] [0] [0] [1] [1] [1] [1]]))
-(def opt-thetas (time (optimize myX myy 0.9 10000 logistic-hypothesis)))
-(println opt-thetas)
-(println "probability of 1 for 0,0" (logistic-hypothesis opt-thetas (alg/matrix [[1 0 0]])))
-(println "probability of 1 for 1,0" (logistic-hypothesis opt-thetas (alg/matrix [[1 1 0]])))
-(println "probability of 1 for 1.5,0" (logistic-hypothesis opt-thetas (alg/matrix [[1 1.5 0]])))
-(println "probability of 1 for 2,0" (logistic-hypothesis opt-thetas (alg/matrix [[1 2 0]])))
-(println "probability of 1 for 3,0" (logistic-hypothesis opt-thetas (alg/matrix [[1 3 0]])))
-(println "probability of 1 for 4,0" (logistic-hypothesis opt-thetas (alg/matrix [[1 4 0]])))
-(println "probability of 1 for 5,0" (logistic-hypothesis opt-thetas (alg/matrix [[1 5 0]])))
-(comment
-)
-
-; Execute a linear test
-(def linX (alg/matrix [[1 0] [1 1] [1 2] [1 3]]))
-(def linY (alg/matrix [0 1 2 3]))
-(def opt-thetas (optimize linX linY 0.09 2000 linear-hypothesis))
-(println opt-thetas)
